052ac18bc285aeb144bd854ba5399a91b3621c64
jean claudio <jean@debian-web>
2019-11-21 11:54:07 -0200

    configurando as Classes Repositories

--- src/main/java/com/jean/registro/repositories/EmpresaRepository.java
+++ src/main/java/com/jean/registro/repositories/EmpresaRepository.java
@@ -1,10 +1,13 @@
 package com.jean.registro.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jean.registro.entities.Empresa;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
 
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
 	
  } 
